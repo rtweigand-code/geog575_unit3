e3a4df2cf8b118cb20e2f6920be0c9dcc41db749
rtweigand-code <rtweigand@wisc.edu>
2026-04-08 22:24:25 -0500

    activity 10

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+var attrArray = ["median_income"];
+var expressed = attrArray[0];
+
 //run script when page loads
 window.onload = setMap;
 
@@ -41,13 +44,149 @@ function setMap(){
         //convert TopoJSON → GeoJSON
         var statesGeo = topojson.feature(states, states.objects.states).features;
 
+        //join csv data to geojson
+        statesGeo = joinData(statesGeo, csvData);
+
+        //make color scale
+        var colorScale = makeColorScale(csvData);
+
         //draw states
         var statesMap = map.selectAll(".states")
             .data(statesGeo)
             .enter()
             .append("path")
             .attr("class", "states")
-            .attr("d", path);
+            .attr("d", path)
+            .style("fill", function(d){
+                var value = d.properties[expressed];
+                if (value){
+                    return colorScale(value);
+                } else {
+                    return "#ccc";
+                }
+            });
+
+        //draw chart
+        setChart(csvData, colorScale);
 
     }
+}
+
+function joinData(geoData, csvData){
+
+    for (var i = 0; i < csvData.length; i++){
+        var csvState = csvData[i];
+        var csvKey = csvState.state;
+
+        for (var a = 0; a < geoData.length; a++){
+            var geoProps = geoData[a].properties;
+            var geoKey = geoProps.name;
+
+            if (geoKey === csvKey){
+
+                attrArray.forEach(function(attr){
+                    var val = parseFloat(csvState[attr]);
+                    geoProps[attr] = val;
+                });
+            }
+        }
+    }
+
+    return geoData;
+}
+
+function makeColorScale(data){
+
+    var colorClasses = [
+        "#edf8fb",
+        "#b2e2e2",
+        "#66c2a4",
+        "#2ca25f",
+        "#006d2c"
+    ];
+
+    var colorScale = d3.scaleQuantile()
+        .range(colorClasses);
+
+    var domainArray = [];
+    for (var i = 0; i < data.length; i++){
+        var val = parseFloat(data[i][expressed]);
+        domainArray.push(val);
+    }
+
+    colorScale.domain(domainArray);
+
+    return colorScale;
+}
+
+function setChart(csvData, colorScale){
+
+    //chart dimensions
+    var chartWidth = 500,
+        chartHeight = 600;
+
+    //create chart svg
+    var chart = d3.select("body")
+        .append("svg")
+        .attr("class", "chart")
+        .attr("width", chartWidth)
+        .attr("height", chartHeight);
+
+    //y scale
+    var yScale = d3.scaleLinear()
+        .range([0, chartHeight - 60])
+        .domain([0, d3.max(csvData, function(d){
+            return parseFloat(d[expressed]);
+        })]);
+
+    //draw bars
+    var bars = chart.selectAll(".bars")
+        .data(csvData)
+        .enter()
+        .append("rect")
+        .sort(function(a, b){
+            return a[expressed] - b[expressed];
+        })
+        .attr("class", "bars")
+        .attr("width", chartWidth / csvData.length - 1)
+        .attr("x", function(d, i){
+            return i * (chartWidth / csvData.length);
+        })
+        .attr("height", function(d){
+            return yScale(parseFloat(d[expressed]));
+        })
+        .attr("y", function(d){
+            return chartHeight - yScale(parseFloat(d[expressed]));
+        })
+        .style("fill", function(d){
+            return colorScale(d[expressed]);
+        });
+
+    //add numbers
+    var numbers = chart.selectAll(".numbers")
+        .data(csvData)
+        .enter()
+        .append("text")
+        .sort(function(a, b){
+            return a[expressed] - b[expressed];
+        })
+        .attr("class", "numbers")
+        .attr("text-anchor", "middle")
+        .attr("x", function(d, i){
+            var fraction = chartWidth / csvData.length;
+            return i * fraction + (fraction - 1) / 2;
+        })
+        .attr("y", function(d){
+            return chartHeight - yScale(parseFloat(d[expressed])) + 15;
+        })
+        .text(function(d){
+            return Math.round(d[expressed]);
+        });
+
+    //add chart title
+    var chartTitle = chart.append("text")
+        .attr("x", 20)
+        .attr("y", 30)
+        .attr("class", "chartTitle")
+        .text("Median Income by State");
 }
