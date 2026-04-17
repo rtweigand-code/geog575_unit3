212164eaa2d5d4929b003b81003a8059292e8a58
rtweigand-code <rtweigand@wisc.edu>
2026-04-17 16:49:00 -0500

    lab 2

--- js/main.js
+++ js/main.js
@@ -1,256 +1,150 @@
-// ================================
+// ======================================
 // GLOBAL VARIABLES
-// ================================
+// ======================================
 
-// all attributes we will use (5 required for lab)
+// variables user can switch between (economic stuff)
 var attrArray = [
     "median_income",
     "unemployment_rate",
     "poverty_rate",
-    "gdp_per_capita",
-    "population_growth"
+    "gdp_per_capita"
 ];
 
-// start with first attribute
+// start with income as default
 var expressed = attrArray[0];
 
-// run when page loads
+// run everything when page loads
 window.onload = setMap;
 
 
-// ================================
-// CREATE MAP + LOAD DATA
-// ================================
-function setMap(){
+// ======================================
+// SET UP MAP + LOAD DATA
+// ======================================
+function setMap() {
 
-    var width = 960,
-        height = 600;
+    var mapWidth = 960,
+        mapHeight = 600;
 
-    // create svg for map
-    var map = d3.select("body")
+    // create the map svg
+    var map = d3.select(".vizRow")
         .append("svg")
         .attr("class", "map")
-        .attr("width", width)
-        .attr("height", height);
+        .attr("width", mapWidth)
+        .attr("height", mapHeight);
 
-    // projection (same as class, works well for US)
+    // projection we’ve been using in class
     var projection = d3.geoAlbersUsa()
         .scale(1200)
-        .translate([width / 2, height / 2]);
+        .translate([mapWidth / 2, mapHeight / 2]);
 
     var path = d3.geoPath()
         .projection(projection);
 
-    // load data
+    // load csv + topojson
     var promises = [];
     promises.push(d3.csv("data/lab3_state_economic_data.csv"));
     promises.push(d3.json("data/states-10m.json"));
 
-    Promise.all(promises).then(callback);
+    Promise.all(promises).then(function(data) {
+        callback(data, map, path);
+    });
 
-    function callback(data){
+    function callback(data, map, path) {
 
         var csvData = data[0];
         var states = data[1];
 
-        // convert numbers (important or everything breaks)
-        csvData.forEach(function(d){
-            attrArray.forEach(function(attr){
-                d[attr] = +d[attr];
-            });
+        // convert strings to numbers (otherwise nothing works right)
+        csvData.forEach(function(d) {
+            d.median_income = +d.median_income;
+            d.unemployment_rate = +d.unemployment_rate;
+            d.poverty_rate = +d.poverty_rate;
+            d.gdp_per_capita = +d.gdp_per_capita;
+            d.population_growth = +d.population_growth;
         });
 
-        // convert topojson → geojson
+        // convert topojson to geojson
         var statesGeo = topojson.feature(states, states.objects.states).features;
 
-        // join csv to geojson
-        statesGeo = joinData(statesGeo, csvData);
+        // join csv data into the geojson
+        joinData(statesGeo, csvData);
 
-        // create color scale
+        // build color scale
         var colorScale = makeColorScale(csvData);
 
         // draw states
-        var statesMap = map.selectAll(".states")
+        map.selectAll(".states")
             .data(statesGeo)
             .enter()
             .append("path")
-            .attr("class", "states")
+            .attr("class", function(d) {
+                return "states " + d.properties.name.replace(/\s+/g, "_");
+            })
             .attr("d", path)
-            .style("fill", function(d){
+            .style("fill", function(d) {
                 var value = d.properties[expressed];
-                return value ? colorScale(value) : "#ccc";
+                return value != null ? colorScale(value) : "#ccc";
+            })
+            .on("mouseover", function(event, d) {
+                highlight(d.properties);
+                setLabel(event, d.properties);
+            })
+            .on("mousemove", function(event) {
+                moveLabel(event);
+            })
+            .on("mouseout", function(event, d) {
+                dehighlight(d.properties);
+                d3.select(".infolabel").style("display", "none");
             });
 
-        // create chart
-        setChart(csvData, colorScale);
-
-        // create dropdown
+        // add dropdown
         createDropdown(csvData);
+
+        // add chart
+        setChart(csvData, colorScale);
     }
 }
 
 
-// ================================
-// JOIN DATA
-// ================================
-function joinData(geoData, csvData){
+// ======================================
+// JOIN CSV DATA TO GEOJSON
+// ======================================
+function joinData(statesGeo, csvData) {
 
-    for (var i = 0; i < csvData.length; i++){
+    for (var i = 0; i < csvData.length; i++) {
         var csvState = csvData[i];
         var csvKey = csvState.state;
 
-        for (var a = 0; a < geoData.length; a++){
-            var geoProps = geoData[a].properties;
+        for (var a = 0; a < statesGeo.length; a++) {
+            var geoProps = statesGeo[a].properties;
             var geoKey = geoProps.name;
 
-            if (geoKey === csvKey){
+            if (geoKey === csvKey) {
 
-                attrArray.forEach(function(attr){
-                    geoProps[attr] = csvState[attr];
-                });
+                geoProps.state = csvState.state;
+                geoProps.abbr = csvState.abbr;
+
+                // attach all attributes so we can use them later
+                geoProps.median_income = csvState.median_income;
+                geoProps.unemployment_rate = csvState.unemployment_rate;
+                geoProps.poverty_rate = csvState.poverty_rate;
+                geoProps.gdp_per_capita = csvState.gdp_per_capita;
+                geoProps.population_growth = csvState.population_growth;
             }
         }
     }
-
-    return geoData;
 }
 
 
-// ================================
-// COLOR SCALE
-// ================================
-function makeColorScale(data){
-
-    var colorClasses = [
-        "#edf8fb",
-        "#b2e2e2",
-        "#66c2a4",
-        "#2ca25f",
-        "#006d2c"
-    ];
-
-    var colorScale = d3.scaleQuantile()
-        .range(colorClasses);
-
-    var domainArray = [];
-
-    for (var i = 0; i < data.length; i++){
-        domainArray.push(data[i][expressed]);
-    }
-
-    colorScale.domain(domainArray);
-
-    return colorScale;
-}
-
-
-// ================================
-// CREATE CHART
-// ================================
-function setChart(csvData, colorScale){
-
-    var chartWidth = 500,
-        chartHeight = 600;
-
-    var chart = d3.select("body")
-        .append("svg")
-        .attr("class", "chart")
-        .attr("width", chartWidth)
-        .attr("height", chartHeight);
-
-    updateChart(csvData, colorScale);
-}
-
-
-// ================================
-// UPDATE CHART
-// ================================
-function updateChart(csvData, colorScale){
-
-    var chartHeight = 600;
-    var chartWidth = 500;
-
-    var yScale = d3.scaleLinear()
-        .range([0, chartHeight - 60])
-        .domain([0, d3.max(csvData, function(d){
-            return d[expressed];
-        })]);
-
-    // sort data
-    csvData.sort(function(a, b){
-        return a[expressed] - b[expressed];
-    });
-
-    // JOIN
-    var bars = d3.selectAll(".bars")
-        .data(csvData);
-
-    // ENTER
-    bars.enter()
-        .append("rect")
-        .attr("class", "bars")
-        .merge(bars)
-        .attr("width", chartWidth / csvData.length - 1)
-        .attr("x", function(d, i){
-            return i * (chartWidth / csvData.length);
-        })
-        .transition()
-        .duration(1000)
-        .attr("height", function(d){
-            return yScale(d[expressed]);
-        })
-        .attr("y", function(d){
-            return chartHeight - yScale(d[expressed]);
-        })
-        .style("fill", function(d){
-            return colorScale(d[expressed]);
-        });
-
-    // labels (cleaner, not cluttered)
-    var labels = d3.selectAll(".numbers")
-        .data(csvData);
-
-    labels.enter()
-        .append("text")
-        .attr("class", "numbers")
-        .merge(labels)
-        .attr("text-anchor", "middle")
-        .attr("x", function(d, i){
-            var fraction = chartWidth / csvData.length;
-            return i * fraction + fraction / 2;
-        })
-        .transition()
-        .duration(1000)
-        .attr("y", function(d){
-            return chartHeight - yScale(d[expressed]) - 5;
-        })
-        .text(function(d, i){
-            return (i % 6 === 0) ? Math.round(d[expressed]) : "";
-        });
-
-    // title
-    var title = d3.selectAll(".chartTitle")
-        .data([expressed]);
-
-    title.enter()
-        .append("text")
-        .attr("class", "chartTitle")
-        .attr("x", 20)
-        .attr("y", 30)
-        .merge(title)
-        .text(expressed.replace(/_/g, " "));
-}
-
-
-// ================================
+// ======================================
 // DROPDOWN
-// ================================
-function createDropdown(csvData){
+// ======================================
+function createDropdown(csvData) {
 
-    var dropdown = d3.select("body")
+    var dropdown = d3.select(".controls")
         .append("select")
         .attr("class", "dropdown")
-        .on("change", function(){
+        .on("change", function() {
             changeAttribute(this.value, csvData);
         });
 
@@ -258,15 +152,170 @@ function createDropdown(csvData){
         .data(attrArray)
         .enter()
         .append("option")
-        .attr("value", function(d){ return d; })
-        .text(function(d){ return d.replace(/_/g, " "); });
+        .attr("value", function(d) { return d; })
+        .text(function(d) { return formatAttributeName(d); });
 }
 
 
-// ================================
-// CHANGE ATTRIBUTE
-// ================================
-function changeAttribute(attribute, csvData){
+// ======================================
+// COLOR SCALE
+// ======================================
+function makeColorScale(data) {
+
+    var colors = [
+        "#edf8fb",
+        "#b2e2e2",
+        "#66c2a4",
+        "#2ca25f",
+        "#006d2c"
+    ];
+
+    var scale = d3.scaleQuantile()
+        .range(colors);
+
+    var domain = [];
+
+    for (var i = 0; i < data.length; i++) {
+        var val = data[i][expressed];
+        if (val != null && !isNaN(val)) {
+            domain.push(val);
+        }
+    }
+
+    scale.domain(domain);
+
+    return scale;
+}
+
+
+// ======================================
+// CREATE CHART
+// ======================================
+function setChart(csvData, colorScale) {
+
+    var chartWidth = 560,
+        chartHeight = 600,
+        leftPadding = 55,
+        rightPadding = 15,
+        topBottomPadding = 50,
+        chartInnerWidth = chartWidth - leftPadding - rightPadding,
+        chartInnerHeight = chartHeight - topBottomPadding * 2;
+
+    var chart = d3.select(".vizRow")
+        .append("svg")
+        .attr("class", "chart")
+        .attr("width", chartWidth)
+        .attr("height", chartHeight);
+
+    // title
+    chart.append("text")
+        .attr("class", "chartTitle")
+        .attr("x", 20)
+        .attr("y", 30);
+
+    // axis group
+    chart.append("g")
+        .attr("class", "axis")
+        .attr("transform", "translate(" + leftPadding + "," + topBottomPadding + ")");
+
+    // create bars
+    chart.selectAll(".bars")
+        .data(csvData)
+        .enter()
+        .append("rect")
+        .attr("class", function(d) {
+            return "bars " + d.state.replace(/\s+/g, "_");
+        })
+        .on("mouseover", function(event, d) {
+            highlight(d);
+            setLabel(event, d);
+        })
+        .on("mousemove", function(event) {
+            moveLabel(event);
+        })
+        .on("mouseout", function(event, d) {
+            dehighlight(d);
+            d3.select(".infolabel").style("display", "none");
+        });
+
+    // x labels (state abbreviations)
+    chart.selectAll(".chartLabels")
+        .data(csvData)
+        .enter()
+        .append("text")
+        .attr("class", function(d) {
+            return "chartLabels " + d.state.replace(/\s+/g, "_");
+        })
+        .attr("text-anchor", "middle");
+
+    updateChart(csvData, colorScale, chartWidth, chartHeight, leftPadding, rightPadding, topBottomPadding, chartInnerWidth, chartInnerHeight);
+}
+
+
+// ======================================
+// UPDATE CHART
+// ======================================
+function updateChart(csvData, colorScale, chartWidth, chartHeight, leftPadding, rightPadding, topBottomPadding, chartInnerWidth, chartInnerHeight) {
+
+    // sort bars based on current variable
+    csvData.sort(function(a, b) {
+        return b[expressed] - a[expressed];
+    });
+
+    var yScale = d3.scaleLinear()
+        .range([chartInnerHeight, 0])
+        .domain([0, d3.max(csvData, function(d) {
+            return d[expressed];
+        }) * 1.05]);
+
+    var yAxis = d3.axisLeft(yScale).ticks(8);
+
+    d3.select(".axis")
+        .transition()
+        .duration(1000)
+        .call(yAxis);
+
+    var barWidth = chartInnerWidth / csvData.length;
+
+    d3.selectAll(".bars")
+        .data(csvData, function(d) { return d.state; })
+        .transition()
+        .duration(1000)
+        .attr("x", function(d, i) {
+            return leftPadding + i * barWidth;
+        })
+        .attr("width", barWidth - 1)
+        .attr("y", function(d) {
+            return topBottomPadding + yScale(d[expressed]);
+        })
+        .attr("height", function(d) {
+            return chartInnerHeight - yScale(d[expressed]);
+        })
+        .style("fill", function(d) {
+            return colorScale(d[expressed]);
+        });
+
+    d3.selectAll(".chartLabels")
+        .data(csvData, function(d) { return d.state; })
+        .transition()
+        .duration(1000)
+        .attr("x", function(d, i) {
+            return leftPadding + i * barWidth + (barWidth / 2);
+        })
+        .attr("y", chartHeight - 12)
+        .text(function(d) {
+            return d.abbr;
+        });
+
+    d3.select(".chartTitle")
+        .text("State Ranking by " + formatAttributeName(expressed));
+}
+
+
+// ======================================
+// DROPDOWN CHANGE
+// ======================================
+function changeAttribute(attribute, csvData) {
 
     expressed = attribute;
 
@@ -276,11 +325,100 @@ function changeAttribute(attribute, csvData){
     d3.selectAll(".states")
         .transition()
         .duration(1000)
-        .style("fill", function(d){
+        .style("fill", function(d) {
             var value = d.properties[expressed];
-            return value ? colorScale(value) : "#ccc";
+            return value != null ? colorScale(value) : "#ccc";
         });
 
     // update chart
-    updateChart(csvData, colorScale);
+    updateChart(csvData, colorScale, 560, 600, 55, 15, 50, 490, 500);
+}
+
+
+// ======================================
+// HIGHLIGHT
+// ======================================
+function highlight(props) {
+
+    var className = props.state
+        ? props.state.replace(/\s+/g, "_")
+        : props.name.replace(/\s+/g, "_");
+
+    d3.selectAll("." + className)
+        .classed("selected", true);
+}
+
+
+// ======================================
+// DEHIGHLIGHT
+// ======================================
+function dehighlight(props) {
+
+    var className = props.state
+        ? props.state.replace(/\s+/g, "_")
+        : props.name.replace(/\s+/g, "_");
+
+    d3.selectAll("." + className)
+        .classed("selected", false);
+}
+
+
+// ======================================
+// TOOLTIP
+// ======================================
+function setLabel(event, props) {
+
+    var labelName =
+        "<strong>" + props.state + "</strong><br>" +
+        formatAttributeName(expressed) + ": " + formatValue(expressed, props[expressed]) + "<br>" +
+        "Population Growth: " + formatValue("population_growth", props.population_growth);
+
+    d3.select(".infolabel")
+        .style("display", "block")
+        .html(labelName);
+
+    moveLabel(event);
+}
+
+
+// move tooltip with mouse
+function moveLabel(event) {
+
+    var x = event.pageX + 15;
+    var y = event.pageY - 60;
+
+    d3.select(".infolabel")
+        .style("left", x + "px")
+        .style("top", y + "px");
+}
+
+
+// ======================================
+// HELPERS
+// ======================================
+
+// make names look clean in dropdown + titles
+function formatAttributeName(attribute) {
+    if (attribute === "median_income") return "Median Income";
+    if (attribute === "unemployment_rate") return "Unemployment Rate";
+    if (attribute === "poverty_rate") return "Poverty Rate";
+    if (attribute === "gdp_per_capita") return "GDP per Capita";
+    if (attribute === "population_growth") return "Population Growth";
+    return attribute;
+}
+
+// format values so they look right ($ vs %)
+function formatValue(attribute, value) {
+
+    if (value == null || isNaN(value)) return "No data";
+
+    if (attribute === "median_income" || attribute === "gdp_per_capita") {
+        return "$" + d3.format(",.0f")(value);
+    }
+
+    if (attribute === "unemployment_rate" || attribute === "poverty_rate" || attribute === "population_growth") {
+        return d3.format(".1f")(value) + "%";
+    }
+
+    return value;
 }
